971213a146064c86567c156aa97d0627d9487e0a
Imran <polboy777@gmail.com>
2023-11-27 19:21:19 -0500

    added build and production configurations

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static("dist"));
+
 app.get("/", (req, res) => {
-  res.json({ message: "Hello!" });
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/api/getAstCode", async (req, res) => {
